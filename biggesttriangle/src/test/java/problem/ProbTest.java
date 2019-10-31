cbeb30943c4e9963f6191ad4206472edefe84097
Warren MacEvoy <wmacevoy@gmail.com>
2019-10-31 11:45:09 -0600

    remove cruft

--- biggesttriangle/src/test/java/problem/ProbTest.java
+++ biggesttriangle/src/test/java/problem/ProbTest.java
@@ -10,106 +10,120 @@ import static problem.Assertions.*;
 import org.junit.jupiter.api.*;
 import java.util.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.Instant;
 
-/**
- *
- * @author wmacevoy
- */
-class TestStream extends PrintStream {
-
-    static TestStream create() {
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        return new TestStream(bos);
-    }
-    final ByteArrayOutputStream bos;
-
-    TestStream(ByteArrayOutputStream bos) {
-        super(bos);
-        this.bos = bos;
-    }
-
-    @Override
-    public void close() {
-        super.close();
-        try {
-            bos.close();
-        } catch (IOException ex) {
-            throw new AssertionError(ex);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return bos.toString();
-    }
-}
-
-class PIO {
-
-    Prob prob;
-    String input, output;
-
-    PIO(Prob prob, String input, String output) {
-        this.prob = prob;
-        this.input = input;
-        this.output = output;
-        prob.in = new Scanner(input);
-        prob.out = TestStream.create();
-    }
-}
 
 public class ProbTest {
 
-    PIO ex1() {
-        Prob prob = new Prob();
+    final String EOL = System.lineSeparator();
+    final double TIME_LIMIT = 3.0;
+
+    class Target extends Prob {
+
+        String input;
+        String answer;
+        ByteArrayOutputStream baos;
+
+        String output() {
+            return baos.toString(StandardCharsets.UTF_8);
+        }
+
+        Target(String input, String answer) {
+            this.input = input.replace("\n", EOL);
+            this.in = new Scanner(input);
+            this.answer = answer.replace("\n", EOL);
+            this.baos = new ByteArrayOutputStream();
+            this.out = new PrintStream(baos, true, StandardCharsets.UTF_8);
+        }
+
+        boolean inputEqual(Prob to) {
+            if (lines.size() != to.lines.size()) {
+                return false;
+            }
+            for (int i = 0; i < lines.size(); ++i) {
+                if (lines.get(i).intersect(to.lines.get(i)) != lines.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        boolean outputEqual(Prob to) {
+            return Math.abs(maxPerimeter - to.maxPerimeter) <= TOLERANCE;
+        }
+
+        double runTime = 0;
+
+        void run() {
+            Instant t0 = Instant.now();
+            super.run();
+            Instant t1 = Instant.now();
+            long nanos = Duration.between(t0, t1).toNanos();
+            runTime = nanos * 1e-9;
+            assertTrue(runTime <= TIME_LIMIT);
+        }
+
+        double solveTime = 0;
+
+        void solve() {
+            Instant t0 = Instant.now();
+            super.solve();
+            Instant t1 = Instant.now();
+            long nanos = Duration.between(t0, t1).toNanos();
+            solveTime = nanos * 1e-9;
+            assertTrue(solveTime <= TIME_LIMIT);
+        }
+    }
+
+    Target ex1() {
+        String input = "3\n"
+                + "0 0 0 1\n"
+                + "0 0 1 0\n"
+                + "0 1 1 0\n";
+        String answer = "3.414214\n";
+        var prob = new Target(input, answer);
         prob.lines.add(new Line(new Point(0, 0), new Point(0, 1)));
         prob.lines.add(new Line(new Point(0, 0), new Point(1, 0)));
         prob.lines.add(new Line(new Point(0, 1), new Point(1, 0)));
         prob.maxPerimeter = 3.4142135624;
-        String EOL = System.lineSeparator();
-        String input = "3" + EOL
-                + "0 0 0 1" + EOL
-                + "0 0 1 0" + EOL
-                + "0 1 1 0" + EOL;
-        String output = "3.414214" + EOL;
-        return new PIO(prob, input, output);
+        return prob;
     }
 
-    PIO ex2() {
-        Prob prob = new Prob();
+    Target ex2() {
+        String input = "3\n"
+                + "0 0 0 1\n"
+                + "0 0 1 0\n"
+                + "0 0 1 1\n";
+        String answer = "no triangle\n";
+        var prob = new Target(input, answer);
         prob.lines.add(new Line(new Point(0, 0), new Point(0, 1)));
         prob.lines.add(new Line(new Point(0, 0), new Point(1, 0)));
         prob.lines.add(new Line(new Point(0, 0), new Point(1, 1)));
         prob.maxPerimeter = 0;
-        String EOL = System.lineSeparator();
-        String input = "3" + EOL
-                + "0 0 0 1" + EOL
-                + "0 0 1 0" + EOL
-                + "0 0 1 1" + EOL;
-        String output = "no triangle" + EOL;
-        return new PIO(prob, input, output);
+        return prob;
     }
 
-    PIO ex3() {
-        Prob prob = new Prob();
+    Target ex3() {
+        String input = "4\n"
+                + "0 0 0 1\n"
+                + "0 4 3 0\n"
+                + "0 0 1 0\n"
+                + "-1 -1 1 1\n";
+        String answer = "12.000000\n";
+        var prob = new Target(input, answer);
         prob.lines.add(new Line(new Point(0, 0), new Point(0, 1)));
         prob.lines.add(new Line(new Point(0, 4), new Point(3, 0)));
         prob.lines.add(new Line(new Point(0, 0), new Point(1, 0)));
         prob.lines.add(new Line(new Point(-1, -1), new Point(1, 1)));
         prob.maxPerimeter = 12;
-        String EOL = System.lineSeparator();
-        String input = "4" + EOL
-                + "0 0 0 1" + EOL
-                + "0 4 3 0" + EOL
-                + "0 0 1 0" + EOL
-                + "-1 -1 1 1" + EOL;
-        String output = "12.000000" + EOL;
-        return new PIO(prob, input, output);
+        return prob;
     }
 
     final int N = 3;
 
-    PIO ex(int k) {
+    Target ex(int k) {
         switch (k) {
             case 1:
                 return ex1();
@@ -122,11 +136,11 @@ public class ProbTest {
     }
 
     void testRead(int k) {
-        PIO expect = ex(k);
-        Prob result = new Prob();
-        result.in = expect.prob.in;
+        var expect = ex(k);
+        var result = new Prob();
+        result.in = expect.in;
         result.read();
-        assertEquals(expect.prob.lines, result.lines, () -> "k=" + k);
+        assertTrue(expect.inputEqual(result), () -> "k=" + k);
     }
 
     @Test
@@ -137,10 +151,9 @@ public class ProbTest {
     }
 
     void testWrite(int k) {
-        PIO pio = ex(k);
-        pio.prob.write();
-        pio.prob.out.close();
-        assertEquals(pio.output, pio.prob.out.toString(), () -> "k=" + k);
+        var prob = ex(k);
+        prob.write();
+        assertEquals(prob.answer, prob.output(), () -> "k=" + k);
     }
 
     @Test
@@ -152,11 +165,10 @@ public class ProbTest {
     }
 
     void testSolve(int k) {
-        PIO expect = ex(k);
-        PIO result = ex(k);
-        result.prob.maxPerimeter = Double.NaN;
-        result.prob.solve();
-        assertEquals(expect.prob.maxPerimeter, result.prob.maxPerimeter, TOLERANCE, () -> "k=" + k);
+        var expect = ex(k);
+        var result = ex(k);
+        result.solve();
+        assertTrue(expect.outputEqual(result), () -> "k=" + k);
     }
 
     @Test
@@ -167,13 +179,9 @@ public class ProbTest {
     }
 
     void testRun(int k) {
-        var pio = ex(k);
-        Prob prob = new Prob();
-        prob.in = pio.prob.in;
-        prob.out = pio.prob.out;
+        var prob = ex(k);
         prob.run();
-        prob.out.close();
-        assertEquals(pio.output, pio.prob.out.toString(), () -> "k=" + k);
+        assertEquals(prob.answer, prob.output(), () -> "k=" + k);
     }
 
     /**
